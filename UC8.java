e2347a8e485be24ea4a8e64c30f764ea965192c9
Soubhagya <singhrajputsoubhagya66@gmail.com>
2026-03-10 13:07:15 +0530

    Allow easy lookup and reuse of letter patterns when building the banner word

--- UC8.java
+++ UC8.java
@@ -3,8 +3,8 @@ import java.util.Map;
 
 class CharacterPattern {
 
-    private char character;
-    private String[] pattern;
+    private final char character;
+    private final String[] pattern;
 
     public CharacterPattern(char character, String[] pattern) {
         this.character = character;
@@ -20,12 +20,15 @@ class CharacterPattern {
     }
 }
 
-public class UC7 {
+class BannerRepository {
 
-    public static void main(String[] args) {
+    private final Map<Character, CharacterPattern> patternMap = new HashMap<>();
 
-        // Store character mappings
-        Map<Character, CharacterPattern> patternMap = new HashMap<>();
+    public BannerRepository() {
+        loadPatterns();
+    }
+
+    private void loadPatterns() {
 
         patternMap.put('O', new CharacterPattern('O', new String[]{
                 " *** ",
@@ -56,15 +59,34 @@ public class UC7 {
                 "    *",
                 "**** "
         }));
+    }
 
-        String text = "OOPS";
+    public CharacterPattern getPattern(char ch) {
+        return patternMap.get(ch);
+    }
+}
 
-        int height = patternMap.get('O').getPattern().length;
+public class UC8 {
+
+    public static void main(String[] args) {
+
+        BannerRepository repository = new BannerRepository();
+        printBanner("OOPS", repository);
+    }
+
+    public static void printBanner(String word, BannerRepository repository) {
+
+        word = word.toUpperCase();
+
+        int height = repository.getPattern(word.charAt(0)).getPattern().length;
 
         for (int i = 0; i < height; i++) {
-            for (char ch : text.toCharArray()) {
-                System.out.print(patternMap.get(ch).getPattern()[i] + "   ");
+
+            for (char ch : word.toCharArray()) {
+                CharacterPattern cp = repository.getPattern(ch);
+                System.out.print(cp.getPattern()[i] + "   ");
             }
+
             System.out.println();
         }
     }
